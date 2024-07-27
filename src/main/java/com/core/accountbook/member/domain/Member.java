f81132a30de59c3ae9c4816ca45757d3c2e14713
Geon.C <chlrjs1324@gmail.com>
2024-07-27 10:40:37 +0900

    feat : Member Aggregate 구현
    - Member : Root Aggregate Member로서의 기본 역할과 책임을 구현, 세부 역할은 하위 Aggregate에 위임
    - Password : 비밀번호 데이터를 가지고 비밀번호와 관련된 세부 기능을 담당하는 Member Aggregate에 속한 VO

--- src/main/java/com/core/accountbook/member/domain/Member.java
+++ src/main/java/com/core/accountbook/member/domain/Member.java
@@ -2,9 +2,7 @@ package com.core.accountbook.member.domain;
 
 import com.core.accountbook.common.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
@@ -18,8 +16,9 @@ public class Member extends BaseEntity {
     @Column(nullable = false)
     private String email;
 
+    @Embedded
     @Column(nullable = false)
-    private String password;
+    private Password password;
 
     @Column(nullable = false)
     private String name;
@@ -34,8 +33,47 @@ public class Member extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Column(name = "login_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private LoginType loginType;
+
     @Column(name = "email_verified")
     private boolean emailVerified;
 
+    @Column(name = "lock_count")
     private int lockCount;
+
+    @Builder
+    private Member(String email,
+                  String password,
+                  String name,
+                  String address,
+                  String phoneNumber,
+                  Role role,
+                  LoginType loginType,
+                  boolean emailVerified,
+                  int lockCount) {
+        this.email = email;
+        this.password = toPassword(password);
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+        this.loginType = loginType;
+        this.emailVerified = emailVerified;
+        this.lockCount = lockCount;
+    }
+
+    public Password toPassword(String password) {
+        return new Password(password);
+    }
+
+    public Member encode(String rowPassword, PasswordEncryptor encoder) {
+        password = this.password.encodePassword(rowPassword, encoder);
+        return this;
+    }
+
+    public boolean checkPassword(String password, PasswordEncryptor passwordEncoder) {
+        return this.password.decode(passwordEncoder).equals(password);
+    }
 }
