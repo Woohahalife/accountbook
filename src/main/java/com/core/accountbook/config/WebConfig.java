90919cd1011756b0b62cc03f942c6e706db2adc6
Geon.C <chlrjs1324@gmail.com>
2024-07-27 10:57:39 +0900

    feat : interceptor, resolver 사용 등록

--- src/main/java/com/core/accountbook/config/WebConfig.java
+++ src/main/java/com/core/accountbook/config/WebConfig.java
@@ -1,21 +1,47 @@
 package com.core.accountbook.config;
 
+import com.core.accountbook.auth.config.AuthArgumentResolver;
+import com.core.accountbook.auth.config.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final String ROOT_PATH = "/api/v1";
+
+    private final AuthInterceptor authInterceptor;
+    private final AuthArgumentResolver authArgumentResolver;
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer
-                .addPathPrefix("/api/v1",
+                .addPathPrefix(ROOT_PATH,
                         HandlerTypePredicate.forBasePackage("com.core.accountbook")
                                 .and(HandlerTypePredicate.forAnnotation(RestController.class)))
                 .setPathMatcher(new AntPathMatcher());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns(ROOT_PATH + "/**")
+                .excludePathPatterns(ROOT_PATH + "/auth/**")
+                .excludePathPatterns(ROOT_PATH + "/member/signup");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authArgumentResolver);
     }
 }
