d46ab7ce1199d375ee0c1331dbc0d5c5a420a099
Geon.C <chlrjs1324@gmail.com>
2024-07-29 18:45:43 +0900

    fix : 비동기 처리 활성화를 위한 @EnableAsync 추가, MailPropertie 주입 활성화

--- src/main/java/com/core/accountbook/AccountbookApplication.java
+++ src/main/java/com/core/accountbook/AccountbookApplication.java
@@ -1,12 +1,15 @@
 package com.core.accountbook;
 
 import com.core.accountbook.auth.domain.TokenProperties;
+import com.core.accountbook.common.properties.SmtpMailProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
-@EnableConfigurationProperties(TokenProperties.class)
+@EnableConfigurationProperties({TokenProperties.class, SmtpMailProperties.class})
 public class AccountbookApplication {
 
 	public static void main(String[] args) {
